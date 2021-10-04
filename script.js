50451c96849326f7da52b928a4ddb2ed919bc39f
Alexis Réau <39715476+Alexisreau97@users.noreply.github.com>
2021-10-04 15:35:10 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -25,3 +25,13 @@ function switchTheme(e) {
 }
 
 toggleSwitch.addEventListener("change", switchTheme, false);
+
+/* CUSTOM CURSOR */
+let mouseCursor = document.querySelector(".cursor");
+
+window.addEventListener('mousemove', cursor);
+
+function cursor(e) {
+  mouseCursor.style.top = e.pageY + "px";
+  mouseCursor.style.left = e.pageX + "px";
+};

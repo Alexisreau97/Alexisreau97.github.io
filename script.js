ba0fac699218ec8ea7ea7556a953b7af2b5b5cbf
Alexis Réau <39715476+Alexisreau97@users.noreply.github.com>
2022-09-10 18:50:11 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+AOS.init();
+
+var realisationSwiper = new Swiper(".realisationSwiper", {
+    slidesPerView: 1,
+    spaceBetween: 40,
+    centeredSlides: false,
+    loop: false,
+    navigation: {
+        nextEl: ".realisationSwiper .swiper-button-next",
+        prevEl: ".realisationSwiper .swiper-button-prev",
+    },
+    // pagination: {
+    //     el: ".realisationSwiper .swiper-pagination",
+    //     clickable: true,
+    // },
+    breakpoints: {
+        576: {
+            slidesPerView: 1,
+            spaceBetween: 40,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+        },
+        992: {
+            slidesPerView: 3,
+            spaceBetween: 40,
+        },
+        1200: {
+            slidesPerView: 3,
+            spaceBetween: 40,
+        }
+    },
+});
